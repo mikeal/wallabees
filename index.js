28c79aeda9023dfe0a76e3f1106cff2c2b159f68
Mikeal Rogers <mikeal.rogers@gmail.com>
2017-11-21 14:33:01 -0800

    feat(cache-control): Honor cache control and expiration headers.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const hasher = require('multihasher')('sha256')
 const bent = require('bent')
 const bl = require('bl')
-
-const get = bent({'User-Agent': 'wallabees-v0.0.0-pre'})
+const { parseCacheControl } = require('wreck')
 
 const getBuffer = stream => {
   return new Promise((resolve, reject) => {
@@ -16,29 +15,68 @@ const getBuffer = stream => {
 }
 
 class Wallabees {
-  constructor (storage) {
+  constructor (storage, minexp = 0) {
     this.storage = storage
+    this.minexp = minexp
+  }
+  expires (meta) {
+    let expiration = 0
+    if (meta.expires) {
+      expiration = (new Date(meta.expires)).getTime()
+      if (isNaN(expiration)) {
+        expiration = 0
+      }
+    }
+    if (meta.control) {
+      if (meta.control['max-age']) {
+        expiration = meta.ts + (meta.control['max-age'] * 100)
+      }
+    }
+    if (expiration < meta.ts + this.minexp) {
+      expiration = meta.ts + this.minexp
+    }
+    return expiration
   }
   async get (_url) {
     let key = await hasher(Buffer.from(_url))
-    let cached = await this.storage.get(key)
-    if (cached) {
-      cached = JSON.parse(cached.toString())
+    let meta = await this.storage.get(key)
+    let headers = {'User-Agent': 'wallabees-v0.0.0-pre'}
+    if (meta) {
+      meta = JSON.parse(meta.toString())
       // TODO: re-do request if cache is out of date.
-      return this.storage.get(cached.body)
-    } else {
-      let res = await get(_url)
-      let body = await getBuffer(res)
-      let bodyKey = await hasher(body)
-      let meta = {
-        url: _url,
-        headers: res.headers,
-        body: bodyKey
+
+      if (this.expires(meta) > Date.now()) {
+        return this.storage.get(meta.body)
       }
-      await this.storage.set(bodyKey, body)
-      await this.storage.set(key, Buffer.from(JSON.stringify(meta)))
-      return body
+
+      if (meta.etag) {
+        headers['if-none-match'] = meta.etag
+      }
+    } else {
+      meta = {url: _url}
     }
+
+    let get = bent(headers, 200, 304)
+    let res = await get(_url)
+    let body
+
+    if (res.statusCode === 304) {
+      body = await this.storage.get(meta.body)
+    } else {
+      /* response was 200, enforced by bent */
+      body = await getBuffer(res)
+      meta.body = await hasher(body)
+      await this.storage.set(meta.body, body)
+    }
+
+    meta.ts = Date.now()
+    meta.expires = res.headers.expires || meta.expires || null
+    meta.etag = res.headers.etag || meta.etag || null
+    meta.control = res.headers['cache-control']
+      ? parseCacheControl(res.headers['cache-control']) : meta.control || null
+
+    await this.storage.set(key, Buffer.from(JSON.stringify(meta)))
+    return body
   }
 }
 
--- tests/test-basics.js
+++ tests/test-basics.js
@@ -28,7 +28,7 @@ const u = path => 'http://localhost:3000' + path
 
 test('basic GET', async t => {
   t.plan(2)
-  let cache = wallabees(inmem)
+  let cache = wallabees(inmem, 1000)
   paths['/basic'] = (req, res) => {
     res.end('ok')
   }
@@ -41,22 +41,23 @@ test('basic GET', async t => {
   t.same(body, body2)
 })
 
-// test('basic GET etag', async t => {
-//   t.plan(2)
-//   let cache = wallabees(inmem)
-//   paths['/etag'] = (req, res) => {
-//     if (req.headers['if-none-match'] === '1234') {
-//       res.stausCode = 304
-//       return res.end()
-//     }
-//     res.setHeader('etag', '1234')
-//     res.end('ok')
-//   }
-//   let body = await cache.get(u('/etag'))
-//   t.same('ok', body.toString())
-//   let body2 = await cache.get(u('/etag'))
-//   t.same(body, body2)
-// })
+test('basic GET etag', async t => {
+  t.plan(3)
+  let cache = wallabees(inmem)
+  paths['/etag'] = (req, res) => {
+    if (req.headers['if-none-match'] === '"1234"') {
+      res.statusCode = 304
+      t.ok(true)
+      return res.end()
+    }
+    res.setHeader('etag', '"1234"')
+    res.end('ok')
+  }
+  let body = await cache.get(u('/etag'))
+  t.same('ok', body.toString())
+  let body2 = await cache.get(u('/etag'))
+  t.same(body, body2)
+})
 
 test('non 200 response', async t => {
   t.plan(2)
@@ -72,4 +73,66 @@ test('non 200 response', async t => {
     t.type(e, 'Error')
     t.same(e.message, 'Incorrect statusCode: 404')
   }
+})
+
+test('expires header', async t => {
+  t.plan(3)
+  let cache = wallabees(inmem)
+  let first = true
+  paths['/expires'] = (req, res) => {
+    t.ok(first)
+    first = false
+    res.setHeader('expires', (new Date(Date.now() + 1000)).toString())
+    res.end('ok')
+  }
+  let body = await cache.get(u('/expires'))
+  t.same('ok', body.toString())
+  let body2 = await cache.get(u('/expires'))
+  t.same(body, body2)
+})
+
+test('cache-control max-age', async t => {
+  t.plan(3)
+  let cache = wallabees(inmem)
+  let first = true
+  paths['/max-age'] = (req, res) => {
+    t.ok(first)
+    first = false
+    res.setHeader('cache-control', 'max-age=60')
+    res.end('ok')
+  }
+  let body = await cache.get(u('/max-age'))
+  t.same('ok', body.toString())
+  let body2 = await cache.get(u('/max-age'))
+  t.same(body, body2)
+})
+
+test('invalid expiration', async t => {
+  t.plan(4)
+  let cache = wallabees(inmem)
+  paths['/invalid-exp'] = (req, res) => {
+    t.ok(true)
+    res.setHeader('expires', 'nope')
+    res.end('ok')
+  }
+  let body = await cache.get(u('/invalid-exp'))
+  t.same('ok', body.toString())
+  let body2 = await cache.get(u('/invalid-exp'))
+  t.same(body, body2)
+})
+
+test('no-cache in cache-control', async t => {
+  t.plan(2)
+  let cache = wallabees(inmem, 1000)
+  paths['/no-cache'] = (req, res) => {
+    res.setHeader('cache-control', 'no-cache')
+    res.end('ok')
+  }
+  let body = await cache.get(u('/no-cache'))
+  t.same('ok', body.toString())
+  paths['/no-cache'] = /* istanbul ignore next */ (req, res) => {
+    res.end('not-ok')
+  }
+  let body2 = await cache.get(u('/no-cache'))
+  t.same(body, body2)
 })
